11fe276f99263cfc6f7f542e75010016d9d5467e
shiwanikumari <001shiwanikumari10@gmail.com>
2025-06-17 09:12:43 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Correct import for React 18
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot instead of render
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
     <App />
